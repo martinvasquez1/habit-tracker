21769800b9bd2ffa9ed2cf56108868071550b0bf
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-03-12 15:51:00 -0300

    Remove spinner styles to fix build error

--- apps/web/src/components/ui/spinner.tsx
+++ apps/web/src/components/ui/spinner.tsx
@@ -5,7 +5,7 @@ export default function Spinner() {
   return (
     <div className="flex justify-center py-20 md:py-14">
       <IconWrapper
-        icon={<LuLoaderCircle className="animate-spin text-primary" />}
+        icon={<LuLoaderCircle />}
         size="md"
       />
       <span className="sr-only">Loading</span>

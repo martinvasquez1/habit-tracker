b47c743370cf1e3459355923a91efe58dd4d76a8
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-03-22 17:22:22 -0300

    Add browser language detection

--- apps/web/src/i18n.ts
+++ apps/web/src/i18n.ts
@@ -4,6 +4,25 @@ import { initReactI18next } from 'react-i18next';
 import en from './locales/en.json';
 import es from './locales/es.json';
 
+function normalizeLanguage(lang: string): string {
+  const fallback = 'en'
+
+  if (!lang) return fallback;
+
+  const shortLang = lang.split('-')[0];
+  if (['en', 'fr', 'es'].includes(shortLang)) return shortLang;
+
+  return 'en';
+}
+
+function getInitialLanguage(): string {
+  const savedLanguage = localStorage.getItem('language');
+  const browserLanguage = normalizeLanguage(navigator.language);
+  const initialLanguage = savedLanguage || browserLanguage;
+
+  return initialLanguage
+}
+
 i18n
   .use(initReactI18next)
   .init({
@@ -11,8 +30,7 @@ i18n
       en: { translation: en },
       es: { translation: es }
     },
-    // Default
-    lng: 'en',
+    lng: getInitialLanguage(),
     fallbackLng: 'en',
     interpolation: { escapeValue: false }
   });

aa240347678bf1c9a60c81839bb00a76c03d76a0
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-04-11 15:34:25 -0400

    refactor(web): render settings from config

--- apps/web/src/pages/settings.tsx
+++ apps/web/src/pages/settings.tsx
@@ -3,9 +3,11 @@ import { useTranslation } from "react-i18next"
 import { jwtDecode } from "jwt-decode";
 
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Label } from "@/components/ui/label";
 import { Container } from "@/components/ui/container";
+import { Item, ItemActions, ItemContent, ItemDescription, ItemTitle } from "@/components/ui/item"
+
 import DeleteUser from "@/features/users/components/delete-user";
+import { Button } from "@/components/ui/button";
 
 export default function Settings() {
     const { t, i18n } = useTranslation()
@@ -26,38 +28,71 @@ export default function Settings() {
         localStorage.setItem("language", lang);
     };
 
+    const items = [
+        {
+            sectionTitle: t('settings.preferences.title'),
+            items: [
+                {
+                    title: t('settings.preferences.language.title'), 
+                    description: t('settings.preferences.language.description'),
+                    component:
+                        <Select
+                            value={language}
+                            onValueChange={handleChange}
+                        >
+                            <SelectTrigger id="language-select">
+                                <SelectValue placeholder="Select language" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="en">{t('settings.preferences.language.options.en')}</SelectItem>
+                                <SelectItem value="es">{t('settings.preferences.language.options.es')}</SelectItem>
+                            </SelectContent>
+                        </Select>,
+                },
+            ]
+        },
+        {
+            sectionTitle: t('settings.account.title'),
+            items: [
+                {
+                    title: t('settings.account.delete.title'),
+                    description: t('settings.account.delete.description'),
+                    component: <DeleteUser id={+userId} />
+                },
+                {
+                    title: t('settings.account.export.title'),
+                    description: t('settings.account.export.description'),
+                    component:<Button variant="outline" size="sm" disabled>{t('settings.account.export.button')}</Button>,
+                }
+            ]
+        }
+    ];
+
+
     return (
         <div>
             <div className="flex justify-between items-baseline">
                 <h1 className="font-bold text-2xl mb-6">{t('settings.title')}</h1>
             </div>
 
-            <Container>
-                <h2 className="font-bold text-2xl mb-2">{t('settings.preferences')}</h2>
-                <div className="flex justify-between gap-4">
-                    <Label htmlFor="language-select">{t('settings.language')}</Label>
-                    <Select
-                        value={language}
-                        onValueChange={handleChange}
-                    >
-                        <SelectTrigger id="language-select">
-                            <SelectValue placeholder="Select language" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectItem value="en">{t('settings.language_options.en')}</SelectItem>
-                            <SelectItem value="es">{t('settings.language_options.es')}</SelectItem>
-                        </SelectContent>
-                    </Select>
-                </div>
-            </Container>
-
-            <Container className="mt-4">
-                <h2 className="font-bold text-2xl mb-2">{t('settings.account')}</h2>
-                <div className="flex justify-between gap-4">
-                    <Label>{t('settings.delete_account')}</Label>
-                    <DeleteUser id={+userId} />
-                </div>
-            </Container>
+            {items.map(item => {
+                return (
+                    <Container className="mb-4">
+                        <h2 className="font-bold text-xl mb-2">{item.sectionTitle}</h2>
+                        {item.items.map(item => {
+                            return (
+                                <Item variant="default" className="px-0">
+                                    <ItemContent>
+                                        <ItemTitle>{item.title}</ItemTitle>
+                                        <ItemDescription>{item.description}</ItemDescription>
+                                    </ItemContent>
+                                    <ItemActions>{item.component}</ItemActions>
+                                </Item>
+                            )
+                        })}
+                    </Container>
+                )
+            })}
         </div>
     )
 }

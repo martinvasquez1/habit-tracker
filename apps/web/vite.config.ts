7a0e893ab28dbcfba77ad08d67ba2de53ac03cd8
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-04-12 21:37:57 -0400

    chore(web): install testing dependencies

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,22 +2,30 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://stackoverflow.com/a/66389044
-export default ({ mode }: { mode: any }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-  return defineConfig({
+  return {
     plugins: [react()],
+
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: './tests/setup.js',
+    },
+
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
       },
     },
+
     server: {
-      port: Number(process.env.VITE_DEV_PORT) || 5173,
+      port: Number(env.VITE_DEV_PORT) || 5173,
     },
+
     preview: {
-      port: Number(process.env.VITE_PREVIEW_PORT) || 4173,
+      port: Number(env.VITE_PREVIEW_PORT) || 4173,
     },
-  });
-};
+  };
+});

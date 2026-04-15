4d8bb5117f5430e2ce938b84106907842758e4a8
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-04-15 17:48:21 -0400

    Add ui package

---
+++ packages/ui/src/gradient.tsx
@@ -0,0 +1,21 @@
+export function Gradient({
+  conic,
+  className,
+  small,
+}: {
+  small?: boolean;
+  conic?: boolean;
+  className?: string;
+}) {
+  return (
+    <span
+      className={`absolute mix-blend-normal will-change-[filter] rounded-[100%] ${
+        small ? "blur-[32px]" : "blur-[75px]"
+      } ${
+        conic
+          ? "bg-gradient-to-r bg-red-500 from-10% via-purple-500 via-30% to-blue-500 to-100%"
+          : ""
+      } ${className ?? ""}`}
+    />
+  );
+}

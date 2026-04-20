5968f59197cd25030daed1dd66198016008f6505
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-04-20 00:21:35 -0400

    marketing(ui): add background lights

--- apps/marketing/components/showcase.tsx
+++ apps/marketing/components/showcase.tsx
@@ -5,7 +5,7 @@ import HeroDark from "@/assets/hero-dark.png"
 
 export default function Showcase() {
   return (
-    <div className="pt-20 max-w-7xl mx-auto">
+    <div className="relative pt-20 max-w-7xl mx-auto">
       <Image
         alt="Hero light"
         src={HeroLight}
@@ -16,6 +16,13 @@ export default function Showcase() {
         src={HeroDark}
         className="w-full hidden dark:block"
       />
+
+      <div className="absolute inset-0 -z-10">
+        <div className="absolute size-60 sm:size-96 lg:size-[600px] bg-habit-red blur-3xl opacity-30 rounded-full top-10 left-10"></div>
+        <div className="absolute size-60 sm:size-96 lg:size-[600px] bg-habit-blue blur-3xl opacity-30 rounded-full bottom-0 right-10"></div>
+        <div className="absolute size-60 sm:size-96 lg:size-[600px] bg-habit-green blur-3xl opacity-30 rounded-full left-10 bottom-0"></div>
+        <div className="absolute size-60 sm:size-96 lg:size-[600px] bg-habit-yellow blur-3xl opacity-30 rounded-full top-20 right-20"></div>
+      </div>
     </div>
   )
 }

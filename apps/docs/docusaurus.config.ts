c29b625d4a3878661b8b2ee16b339b962aa9e378
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-05-07 16:46:18 -0400

    chore(docs): update docusaurus config

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -15,15 +15,18 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://martinvasquez1.github.io',
+
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/habit-tracker/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'martinvasquez1', // Usually your GitHub org/user name.
+  projectName: 'habit-tracker', // Usually your repo name.
+
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
 

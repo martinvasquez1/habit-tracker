f014732e07def6e8447d0077cc957dcb4cf50bd7
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-05-07 16:29:04 -0400

    chore(web): remove ul background color

--- apps/web/src/layouts/navbar.tsx
+++ apps/web/src/layouts/navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar() {
         </Button>
         <MobileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
         <div className="hidden w-full md:block md:w-auto">
-          <ul className="font-medium flex items-center p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex items-center p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
             {links.map((link, index) => {
               if (link.path === "/sign-up") {
                 return (

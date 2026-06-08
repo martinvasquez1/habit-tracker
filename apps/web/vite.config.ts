00e53ae94863392f695ba2de7aca32004ff2aa38
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-06-07 21:16:35 -0400

    feat(web): add rollup-plugin-visualizer

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv, PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react()],
+    plugins: [react(), visualizer({ template: 'flamegraph' }) as PluginOption],
 
     test: {
       globals: true,
